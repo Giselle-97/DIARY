e555ed3bb83288231eb703d23d826bec9ad8815d
Giselle-97 <giselle.cduran@hotmail.com>
2023-07-13 12:15:36 +0200

    updated the form

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,67 +1,77 @@
 import '../../styles/pages/landing.scss';
-import { Link } from 'react-router-dom';
-import imgDiary from '../../images/222.png';
-import imgLogo from '../../images/Giselle.png';
-//import imgIntagram from '../../images/instagram.png';
-//import imgFacebook from '../../images/facebook.png';
-//import imgLinkedin from '../../images/linkedin0 (1).png';
+//import { Link } from 'react-router-dom';
+//import imgDiary from '../../images/222.png';
+//import imgLogo from '../../images/Giselle.png';
+import imgInstagram from '../../images/instagram.png';
+import imgFacebook from '../../images/facebook.png';
+import imgLinkedin from '../../images/linkedin0 (1).png';
 
 function Landing() {
+  /*EVENTO CLIK
+  hay que traer btnregistro, btn iniciar s, contenedor registro, contenedor inicar sesión"
+  if(e.target===$btnSingIn || e.target === $btnSingUp){
+    $singIn.classList.toggle('active);
+    $sinUp.classList.toggle('active')
+  }
+  */
   return (
     <div className='containerLanding'>
-      <header className='header'>
-        <img className='header__img' src={imgDiary} alt='logo diary' />
-        <p className='header__text'>
-          "El secreto del ÉXITO es organizar bien tu TIEMPO"
-        </p>
-      </header>
       <main className='hero'>
-        <div className='divHero'>
-          <form className='container'>
-            <fieldset className='container__login'>
-              <legend className='legend'>Crea tu perfil</legend>
-              <div className='container__input'>
-                <label className='userLabel' htmlFor='user'>
-                  Usuario
-                </label>
-                <input
-                  className='user'
-                  type='text'
-                  name='user'
-                  placeholder='nina@gmail.com'
-                />
+        <div className='containerForm signUp'>
+          <div className='welcomeBack'>
+            <div className='message'>
+              <h2>Bienvenido a Diary</h2>
+              <p>Si ya tienes una cuenta por favor inicia sesión aquí</p>
+              <button className='signUpBtn'>Iniciar sesión</button>
+            </div>
+          </div>
+          <form className='formL'>
+            <p className='createAccount'>Crear una cuenta</p>
+            <div className='icons'>
+              <div className='borderIcon iconInstagram'>
+                <img src={imgInstagram} alt='logo instagram' className='icon' />
               </div>
-              <div className='container__input'>
-                <label className='userLabel' htmlFor='password'>
-                  Contraseña
-                </label>
-                <input
-                  className='password'
-                  type='text'
-                  name='password'
-                  placeholder='nina555'
-                />
+              <div className='borderIcon iconFacebook'>
+                <img src={imgFacebook} alt='logo facebook' className='icon' />
               </div>
-              <div className='divBtn'>
-                <button className='btn'>Registrarse</button>
+              <div className='borderIcon iconLinkedin'>
+                <img src={imgLinkedin} alt='logo linkedin' className='icon' />
               </div>
-            </fieldset>
+            </div>
+            <input type='text' placeholder='name' />
+            <input type='email' placeholder='email' />
+            <input type='password' placeholder='password' />
+            <input type='button' value='Registrarse' className='inputBtn' />
+          </form>
+        </div>
+        <div className='containerForm signIn'>
+          <div className='welcomeBack'>
+            <div className='message'>
+              <h2>Bienvenido de nuevo</h2>
+              <p>Si aún no tienes una cuenta por favor registrese aquí</p>
+              <button className='signInBtn'>Registrarse</button>
+            </div>
+          </div>
+          <form className='formL'>
+            <p className='createAccount'>Iniciar sesión</p>
+            <div className='icons'>
+              <div className='borderIcon iconInstagram'>
+                <img src={imgInstagram} alt='logo instagram' className='icon' />
+              </div>
+              <div className='borderIcon iconFacebook'>
+                <img src={imgFacebook} alt='logo facebook' className='icon' />
+              </div>
+              <div className='borderIcon iconLinkedin'>
+                <img src={imgLinkedin} alt='logo linkedin' className='icon' />
+              </div>
+            </div>
+
+            <input type='email' placeholder='email' />
+            <input type='password' placeholder='password' />
+            <input type='button' value='Iniciar sesión' className='inputBtn' />
           </form>
         </div>
       </main>
-      <footer className='footer'>
-        <div className='divFooter'>
-          <nav>
-            <Link className='divFooter__btn' to='/contact'>
-              Contacto
-            </Link>
-            <Link to='/home' className='divReturn__text'>
-              Home
-            </Link>
-          </nav>
-          <img className='divFooter__img' src={imgLogo} alt='logo' />
-        </div>
-      </footer>
     </div>
   );
 }
