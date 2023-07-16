9e844fff2cd2311cafa2974439de492321ac37ff
Giselle-97 <giselle.cduran@hotmail.com>
2023-07-16 20:36:50 +0200

    added new changes and styles in the contact form

--- src/components/pages/FormContact.js
+++ src/components/pages/FormContact.js
@@ -3,42 +3,45 @@ import '../../styles/pages/formContact.scss';
 function FormContact() {
   return (
     <div className='containerFormC'>
-      <form class='formContact'>
-        <div className='divInputC'>
-          <input type='text' required />
-          <label class='lblName'>
-            <span class='textName'>Nombre</span>
-          </label>
-        </div>
+      <div className='form-box'>
+        <form className='formContact'>
+          <h2 className='titleDateContact'>Datos de Contacto</h2>
+          <div className='divInputC'>
+            <input type='text' required />
+            <label className='lblName'>
+              <span className='textName'>Nombre</span>
+            </label>
+          </div>
 
-        <div>
-          <input type='text' required />
-          <label class='lblLastName'>
-            <span class='textLastName'>Apellidos</span>
-          </label>
-        </div>
+          <div className='divInputC'>
+            <input type='text' required />
+            <label className='lblLastName'>
+              <span className='textLastName'>Apellidos</span>
+            </label>
+          </div>
 
-        <div>
-          <input type='email' required />
-          <label class='lblEmail'>
-            <span class='textEmail'>Correo eletrónico</span>
-          </label>
-        </div>
+          <div className='divInputC'>
+            <input type='email' required />
+            <label className='lblEmail'>
+              <span className='textEmail'>Correo eletrónico</span>
+            </label>
+          </div>
 
-        <div>
-          <input type='phone' required />
-          <label class='lblPhone'>
-            <span class='textPhone'>Teléfono</span>
-          </label>
-        </div>
+          <div className='divInputC'>
+            <input type='phone' required />
+            <label className='lblPhone'>
+              <span className='textPhone'>Teléfono</span>
+            </label>
+          </div>
 
-        <div>
-          <input type='questions' required />
-          <label class='lblQuestions'>
-            <span class='textQuestions'>¿En qué le puedo ayudar?</span>
-          </label>
-        </div>
-      </form>
+          <div className='divInputC'>
+            <input type='questions' required />
+            <label className='lblQuestions'>
+              <span className='textQuestions'>¿En qué le puedo ayudar?</span>
+            </label>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
